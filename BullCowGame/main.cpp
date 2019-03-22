c144a2512f25db965e18667ff65df6bdc4d92fe6
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-21 22:57:21 -0400

    Created input variable and to get input from user. and return the inputed value back to the user.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-	cout << "Welcome to Bulls and Cows\n";
+	// introduce the game
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH; 
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	
+
+	//repeat the guess back to them
+	cout << endl;
+	cout << "Your guess was: " << Guess;
+
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+
+
+	//repeat the guess back to them
+	cout << endl;
+	cout << "Your guess was: " << Guess;
+
 	return 0;
 }

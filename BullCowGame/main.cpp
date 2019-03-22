0098ff600d908cee8565b26fbc9afdf43822665d
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-21 23:25:32 -0400

    Simplified code by writing methods

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,33 +3,41 @@
 
 using namespace std;
 
+
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
+// the entry point for the applicaiton
 int main() {
 
-	// introduce the game
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	
+	cout << endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH; 
+	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
-	//get a guess from the player
+//get a guess from the player
+string GetGuessAndPrintBack() {
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	
-
-	//repeat the guess back to them
+	//print the guess back to them
 	cout << endl;
 	cout << "Your guess was: " << Guess;
-
-	//get a guess from the player
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-
-	//repeat the guess back to them
-	cout << endl;
-	cout << "Your guess was: " << Guess;
-
-	return 0;
+	return Guess;
 }

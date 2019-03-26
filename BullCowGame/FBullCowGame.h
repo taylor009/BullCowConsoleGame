293fe0547934b7fc518977efd839540ec2f7ae35
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-26 11:31:45 -0400

    added more classes

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -4,20 +4,48 @@
 
 
 
-class FBullCowGame {
+// to make syntax Unreal friendly
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame
+{
 public:
-	FBullCowGame(); //constructor 
+	FBullCowGame(); // constructor
 
-	int GetMaxTries() const;
-	int GetCurrentTry() const;
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
 	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
 
-	void Reset(); //TODO make a rich return value.
-	bool CheckGuessValidity(std::string); // TODO make a more rich return value
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
 
 
-
+	// ^^ Please try and ignore this and focus on the interface above ^^
 private:
-	int MyCurrentTry;
-	int MyMaxTries;
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
 };

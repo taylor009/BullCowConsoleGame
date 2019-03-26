d3b636c682a2f319c581b0163b2fe68c58222a93
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-26 11:24:03 -0400

    Created constant methods

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -1,15 +1,16 @@
 #include "FBullCowGame.h"
 
-int FBullCowGame::GetMaxTries() { return MyMaxTries; }
+int FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 
-int FBullCowGame::GetCurrentTry() { return MyCurrentTry; }
+int FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 
 
 void FBullCowGame::Reset()
 {
+	return
 }
 
-bool FBullCowGame::IsGameWon()
+bool FBullCowGame::IsGameWon() const
 {
 	return false;
 }

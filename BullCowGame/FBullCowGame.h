cee8cabc107fdfad4777446b9417033c2f8185b1
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-25 23:52:45 -0400

    Created class for game

---
+++ BullCowGame/FBullCowGame.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class FBullCowGame {
+public:
+	void Reset(); //TODO make a rich return value.
+	int GetMaxTries();
+	int GetCurrentTry;
+	bool IsGameWon();
+	bool CheckGuessValidity(string); // TODO make a more rich return value
+
+
+
+private:
+	int MyCurrentTry;
+	int MyMaxTries;
+};

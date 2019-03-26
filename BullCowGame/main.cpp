09701645914c69dd139b9f8956cad4a42a8d6dbf
Taylor Gagne <taylor@baremetalcloud.com>
2019-03-26 11:06:58 -0400

    add an instantiation of class

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include "FBullCowGame.h"
 
 
 void PrintIntro();
@@ -39,6 +39,8 @@ void PrintIntro()
 
 void PlayGame()
 {
+	FBullCowGame BCGame; // instantiate a new game
+
 	// loop for the number of turns asking for guesses 
 	constexpr int NUMBER_OF_TURNS = 5;
 	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
@@ -55,6 +57,7 @@ void PlayGame()
 	//get a guess from the player
 	std::cout << "Enter your guess: ";
 	std::string Guess = "";
+	FBullCowGame BCGame
 	std::getline(std::cin, Guess);
 	return;
 }
